724dff87e97599538e9d4183fa996862d84c4389
Martin Zacho <hi@martinzacho.net>
2023-05-11 18:08:12 +0200

    refactor test framework a bit

--- src/tests/test_secwasm.ml
+++ src/tests/test_secwasm.ml
@@ -1,8 +1,18 @@
 open Secwasm.Ast
 open Secwasm.Type_check
 
-let print_test_result test_id result = 
-  print_endline ("Test " ^ test_id ^ ": " ^ (if result then "Success" else "Failure"))
+let next_id =
+  let counter = ref 0 in
+  fun () ->
+    incr counter;
+    Printf.sprintf "%d" !counter
+
+let print_test_result name result =
+  let s = if result then "SUCCESS" else "FAIL" in
+  print_endline ("Test" ^ next_id () ^ " [" ^ name ^ "]\t\t: " ^ s)
+
+let test_check_module (name : string) (m : wasm_module) =
+  print_test_result name (type_check_module m)
 
 (*
   (module 
@@ -14,31 +24,42 @@ let print_test_result test_id result =
     )
   )
 *)
-let module1 = 
+let module1_add_consts =
   {
     globals = [];
-    functions = [
-      {
-        ftype = FunType ([], [I32]); 
-        locals = []; 
-        body = [WI_Const 1l; WI_Const 1l; WI_BinOp Add]; 
-        export_name = None
-      }
-    ]
+    functions =
+      [
+        {
+          ftype = FunType ([], [ I32 ]);
+          locals = [];
+          body = [ WI_Const 1l; WI_Const 1l; WI_BinOp Add ];
+          export_name = None;
+        };
+      ];
   }
 
-let test1 = 
-  fun () -> 
-    let result = type_check_module module1 in 
-    print_test_result "test1" result
+(*
+  (module
+    (func
+      (local i32)
+      i32.const 42
+      local.set 0
+    )
+  )
+*)
+let module2_local_set =
+  {
+    globals = [];
+    functions =
+      [
+        {
+          ftype = FunType ([], []);
+          locals = [ I32 ];
+          body = [ WI_Const 42l; WI_LocalSet 0l ];
+          export_name = None;
+        };
+      ];
+  }
 
-
-let test2 = 
-  fun () -> print_test_result "test2" false
-
-let () = 
-  test1();
-  test2();
-  ()
-
-
+let _ = test_check_module "add consts" module1_add_consts
+let _ = test_check_module "local.set" module2_local_set
