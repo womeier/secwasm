eb74a710f8885c16fc79c5b5dba35cd03ed59f73
Martin Zacho <hi@martinzacho.net>
2023-05-18 19:24:02 +0200

    tests: Add block-2 negative test

--- src/tests/test_secwasm.ml
+++ src/tests/test_secwasm.ml
@@ -959,7 +959,7 @@ let _ =
 (*
   Test block typechecks
 
-  (func (export "foo")
+  (func
   (param i32) (result i32)
       local.get 0
       block (param i32) (result i32)
@@ -969,6 +969,47 @@ let _ =
 *)
 let _ =
   test "block-1" pos_test
+    {
+      memories = [];
+      globals = [];
+      functions =
+        [
+          {
+            ftype =
+              FunType
+                ( [ { t = I32; lbl = Public } ],
+                  Public,
+                  [ { t = I32; lbl = Public } ] );
+            locals = [ { t = I32; lbl = Public } ];
+            body =
+              [
+                WI_LocalGet 0;
+                WI_Block
+                  ( BlockType
+                      ( [ { t = I32; lbl = Public } ],
+                        [ { t = I32; lbl = Public } ] ),
+                    [ WI_Nop ] );
+              ];
+            export_name = None;
+          };
+        ];
+    }
+
+(*
+  Test type mismatch at end of block, expected [] but got [i32]
+
+  (func
+  (param i32) (result i32)
+      local.get 0
+      block (param i32)
+        nop
+      end
+  )
+*)
+
+let _ =
+  test "block-2"
+    (neg_test (TypingError "bar"))
     {
       memories = [];
       globals = [];
