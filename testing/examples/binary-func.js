516638166a044865afd970f8e089e29b1f187d8c
Martin Zacho <hi@martinzacho.net>
2023-05-30 10:12:46 +0200

    run binary

---
+++ testing/examples/binary-func.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const bytes = fs.readFileSync(__dirname + "/" + process.argv[2]);
+const val_1 = process.argv[3];
+const val_2 = process.argv[4];
+
+let importObject = {
+    env: {
+        log_i32: (value) => { console.log("logging i32: ", value) },
+        log_i64: (value) => { console.log("logging i64: ", value) },
+    },
+};
+
+(async () => {
+    const obj = await WebAssembly.instantiate(
+        new Uint8Array(bytes), importObject
+    );
+
+    let res = obj.instance.exports.foo(val_1, val_2);
+    console.log(`===> ${res}`);
+})();
